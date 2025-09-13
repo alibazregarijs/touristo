4d51a5cd8580818738e128313ecba6cd633a4651
alibazregarijs <alibazregariintel@gmail.com>
2025-09-13 09:46:13 +0330

    main:color palate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,14 @@ const config: Config = {
         sans: ['var(--font-inter)', 'system-ui', 'sans-serif'], // external
         vazir: ['var(--font-vazir)', 'sans-serif'], // local
       },
+      colors: {
+        'black-1': '#19191c',
+        'pink-1': '#70243c',
+        'pink-2': '#fd366e',
+        'pink-3': '#2c1b24',
+        'white-1': '#c3c3c6',
+        'white-2': '#a0a0a4',
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
